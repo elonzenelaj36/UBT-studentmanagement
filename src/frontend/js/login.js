28d2433e24e759241cc3a5dc34cca7ec406b2ce9
elonzenelaj36 <elonzenelaj36@gmail.com>
2025-12-07 18:18:40 +0100

    login+validimi

--- src/frontend/js/login.js
+++ src/frontend/js/login.js
@@ -1,3 +1,19 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", function(event){
+    event.preventDefault();
+ 
+    const savedUser = JSON.parse(localStorage.getItem("userData")); //marrja e te dhenave qe jan regjistruar ne register.js(localStorage)
+
+    const username = document.getElementById("loginUsername").value; //marrja e te dhenave nga forma e login(ato qe i shkruan perdoruesi)
+    const password = document.getElementById("loginPassword").value;
+ 
+    if(savedUser && username === savedUser.username && password === savedUser.password){ //kontrolli nese te dhenat e futura perputhen me ato te regjistruara
+        alert("Login i suksesshëm!");
+    } else {
+        alert("Username ose password gabim!");
+    }
+});
 
 
   
--- src/frontend/js/register.js
+++ src/frontend/js/register.js
@@ -1,19 +1,19 @@
 
 
-const form = document.getElementById("registerForm");
+const form = document.getElementById("registerForm"); // burimi i te dhenave
 
  form.addEventListener("submit", function(event){
 
-      event.preventDefault();
+      event.preventDefault();     // me parandalu rifreskimin e faqes
 
-      localStorage.setItem("userData", JSON.stringify({
+      localStorage.setItem("userData", JSON.stringify({  // ruajme te dhenat ne local storage(JSON.stringify per konvertim ne string)
         name : document.getElementById("name").value,
         lname: document.getElementById("lastname").value,
         username : document.getElementById("username").value,
         password : document.getElementById("password").value
       }))
 
-      console.log(JSON.parse(localStorage.getItem("userData")));
+      console.log(JSON.parse(localStorage.getItem("userData"))); // merren te dhenat nga local storage(JSON.parse i kthen prap ne objekt)
 
      alert("Ju jeni regjistruar!")
  });
