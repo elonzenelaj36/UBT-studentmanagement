d6dd5da76c85ff55004834a27618f56debd48952
elonzenelaj36 <elonzenelaj36@gmail.com>
2026-01-31 23:32:09 +0100

    test

--- src/frontend/js/register.js
+++ src/frontend/js/register.js
@@ -1,85 +1,85 @@
-// document.getElementById("registerForm").addEventListener("submit", function (e) {
-//     e.preventDefault();
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-//     const name = document.getElementById("name");
-//     const surname = document.getElementById("surname");
-//     const username = document.getElementById("username");
-//       const email = document.getElementById("email");
-//     const password = document.getElementById("password");
+    const name = document.getElementById("name");
+    const surname = document.getElementById("surname");
+    const username = document.getElementById("username");
+      const email = document.getElementById("email");
+    const password = document.getElementById("password");
 
-//     const nameRegex = /^[A-Za-z]{2,}$/;
-//     const usernameRegex = /^[a-zA-Z0-9_]{4,15}$/;
-//    // const passwordRegex =
-// //        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
+    const nameRegex = /^[A-Za-z]{2,}$/;
+    const usernameRegex = /^[a-zA-Z0-9_]{4,15}$/;
+   // const passwordRegex =
+//        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
 
-//     const errors = {
-//         name: document.getElementById("nameError"),
-//         surname: document.getElementById("surnameError"),
-//         email: document.getElementById("emailError"),
-//         username: document.getElementById("usernameError"),
-//         password: document.getElementById("passwordError")
-//     };
+    const errors = {
+        name: document.getElementById("nameError"),
+        surname: document.getElementById("surnameError"),
+        email: document.getElementById("emailError"),
+        username: document.getElementById("usernameError"),
+        password: document.getElementById("passwordError")
+    };
 
-//    // Object.values(errors).forEach(e => e.textContent = "");
+   // Object.values(errors).forEach(e => e.textContent = "");
 
 
-//     [name, surname, email, username, password].forEach(i =>
-//         i.classList.remove("error-input")
-//     );
+    [name, surname, email, username, password].forEach(i =>
+        i.classList.remove("error-input")
+    );
 
-//     let isValid = true;
+    let isValid = true;
 
-// if (!nameRegex.test(surname.value.trim())) {
-//     errors.surname.textContent = "Mbiemri duhet te ket se paku 2 shkronja!";
-//     surname.classList.add("error-input");
-//     isValid = false;
-// }
+if (!nameRegex.test(surname.value.trim())) {
+    errors.surname.textContent = "Mbiemri duhet te ket se paku 2 shkronja!";
+    surname.classList.add("error-input");
+    isValid = false;
+}
 
-//     if (!nameRegex.test(surname.value.trim())) {
-//         errors.lastname.textContent = "Mbiemri duhet te ket se paku 2 shkronja!";
-//         lastname.classList.add("error-input");
-//         isValid = false;
-//     }
+    if (!nameRegex.test(surname.value.trim())) {
+        errors.lastname.textContent = "Mbiemri duhet te ket se paku 2 shkronja!";
+        lastname.classList.add("error-input");
+        isValid = false;
+    }
 
-//     if (!usernameRegex.test(username.value.trim())) {
-//         errors.username.textContent =
-//             "Username duhet te ket 4–15 karaktere dhe duhet te permbaj shkronja, numra, ose nenvije!.";
-//         username.classList.add("error-input");
-//         isValid = false;
-//     }
+    if (!usernameRegex.test(username.value.trim())) {
+        errors.username.textContent =
+            "Username duhet te ket 4–15 karaktere dhe duhet te permbaj shkronja, numra, ose nenvije!.";
+        username.classList.add("error-input");
+        isValid = false;
+    }
 
-//    if (!passwordRegex.test(password.value)) {
-//         errors.password.textContent =
-//             "Fjalëkalimi duhet të ketë të paktën 8 karaktere dhe të përfshijë shkronja të mëdha, shkronja të vogla, numra dhe një karakter special.";
-//         password.classList.add("error-input");
-//         isValid = false;
-//     } 
+   if (!passwordRegex.test(password.value)) {
+        errors.password.textContent =
+            "Fjalëkalimi duhet të ketë të paktën 8 karaktere dhe të përfshijë shkronja të mëdha, shkronja të vogla, numra dhe një karakter special.";
+        password.classList.add("error-input");
+        isValid = false;
+    } 
 
-//     if (!isValid) return;
+    if (!isValid) return;
 
-// const formData = new FormData(document.getElementById("registerForm"));
+const formData = new FormData(document.getElementById("registerForm"));
 
-//     fetch('/UBT-studentmanagement/src/backend/api/UserController.php', {
-//         method: 'POST',
-//         body: formData
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         if(data.success){
-//      alert("registered");
-//         }
-//        else{
+    fetch('/UBT-studentmanagement/src/backend/api/UserController.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.success){
+     alert("registered");
+        }
+       else{
         
-//         alert("failed to register");
-//         }
-//     })
-//     .catch(err => {
-//        alert(err);
-//         console.error("Fetch error:", err);
-//     });
+        alert("failed to register");
+        }
+    })
+    .catch(err => {
+       alert(err);
+        console.error("Fetch error:", err);
+    });
 
-//     e.target.reset();
-// });
+    e.target.reset();
+});
 
 
 
